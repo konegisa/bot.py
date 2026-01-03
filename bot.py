249e204422b3bb1c051fd8d830aea8665e34fdee
konegisa <senturkahmed80@gmail.com>
2026-01-03 19:44:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -179,7 +179,8 @@ async def on_message(msg):
 # ======================================================
 # TOKEN
 # ======================================================
-bot.run(os.getenv("MTQwODIwNDk3NTI4NDE2Mjc0Mg.Ga_VlV.hSNfJV_MN4fUPdZD4zw6I5hiuUTFMp1atCvbug"))
+bot.run(os.getenv("TOKEN"))
+
 
 
 
